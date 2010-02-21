f64d54f2c6f226b2ba42cf15b4b17d1fe433581e
Peter Boraros <pborky@pborky.sk>
2010-02-21 19:18:09 +0000

    Made: FrontControllerFactory..

--- webroot/wsgitest.py
+++ webroot/wsgitest.py
@@ -4,4 +4,4 @@ __date__ ="$18.2.2010 16:30:21$"
 import wsgi.demo.myapp as app
 import wsgi.controller as ctrl
 
-application = ctrl.FrontController(app.MyApp)
+application = ctrl.FrontControllerFactory.produce(app.MyApp())
--- wsgi/controller.py
+++ wsgi/controller.py
@@ -1,53 +1,78 @@
 __author__="pborky"
 __date__ ="$18.2.2010 16:41:24$"
 
+# TODO: refactor this module
+
+import sys
+
+
+class AppNotInitializedProperly(Exception):
+    pass
+
+class FrontControllerFactory(object):
+    _controller = None
+    _init_exc_info = None
+    
+    @staticmethod
+    def produce(app):
+        if (controller == None):
+            return _FrontControllerHandler(None, init_exc_info)
+        else:
+            controller.set_app(app)
+            return _FrontControllerHandler(controller, None)
+
+
+class _FrontControllerHandler(object):
+    """ This is fail safe front controller. It returns 500 and error details"""
+    # TODO: make confirable the error output
+    def __init__(self, controller, init_exc_info):
+        self._controller = controller
+        self._init_exc_info = init_exc_info
+
+    def __call__(self, environ, start_resp):
+        try:
+            if (self._controller == None):
+                raise AppNotInitializedProperly()
+            else:
+                return self._controller(environ, start_resp)
+        except:
+            fail(start_resp, sys.exc_info())
+
+    def fail(self, start_resp, exc_info):
+        start_resp('500 Internal Server Error', [('Content-type', 'text/html')], exc_info)
+        return ['<h1>500 Internal Server Error</h1>',
+                '<p>Server initialiazation failed.</p>',
+                '<code>',
+                '<p>', exc_info[0].__name__, ':</br> ',
+                str(exc_info[1]), '</p>',
+                '</code>'
+                ]
+
 try:
     from wsgi import context as ctx
 
-    class FrontController(object):
+    class _FrontControllerWorker(object):
 
-        def __init__(self, AppClass):
+        def __init__(self):
             self._sess = ctx.Session()
-            self._app = AppClass()
+            self._app = None
+
+        def set_app(self, instance):
+            self._app = instance
 
         def __call__(self, environ, start_response):
-            try:
-                context = ctx.Context(self._sess, ctx.Request(environ), ctx.Response(start_response))
+            context = ctx.Context(self._sess, ctx.Request(environ), ctx.Response(start_response))
 
+            if (self._app == None):
+                raise AppNotInitializedProperly('Missing application to execute.')
+            else:
                 self._app(context)
 
-                return context.response.return_response()
+            return context.response.return_response()
 
-            except Exception as inst:
-                raise
-                return ctx.Response(start_response,
-                                    500,
-                                    [('Content-Type', 'text/html')],
-                                    [ '<h1>500 Internal Server Error</h1>',
-                                      'Raised <b>',
-                                      str(inst.__class__.__name__),
-                                      '</b> with argument(s): <b>',
-                                      str(inst),
-                                      '</b>.']
-                                   ).return_response()
+    # TODO: this should be diferent - think about
+    FrontControllerFactory._controller = _FrontControllerWorker()
 except:
-    import sys
-
-    class FrontControllerInitFailed(object):
-        """ This is fail safe front controller. It returns 500 and error details"""
-        # TODO: make confirable the error output
-        def __init__(self, AppClass):
-            self._exc_info = sys.exc_info()
-
-        def __call__(self, environ, start_resp):
-            start_resp('500 Internal Server Error', [('Content-type', 'text/html')], self._exc_info)
-
-            return ['<h1>500 Internal Server Error</h1>',
-                    '<p>Server initialiazation failed.</p>',
-                    '<code>',
-                    '<p>', self._exc_info[0].__name__, ':</br> ',
-                    str(self._exc_info[1]), '</p>',
-                    '</code>'
-                    ]
-    # TODO: this should be diferent - factory class generating front controller
-    FrontController = FrontControllerInitFailed
+    # TODO: this should be diferent - think about
+    FrontControllerFactory._init_exc_info = sys.exc_info()
+    FrontControllerFactory._controller = None
