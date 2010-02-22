9b150554c7220af6e4d2e20bc64e7db1247b6d87
Peter Boraros <pborky@pborky.sk>
2010-02-22 14:05:01 +0000

    Fix incompatibility with python 2.5 (properties)

--- wsgi/context.py
+++ wsgi/context.py
@@ -9,26 +9,22 @@ class Request:
 
     def __init__(self, environ):
         """ Initiate Request object """
-        self._environ = environ
+        self.environ = environ
         lines = []
         for line in environ['wsgi.input']:
             lines.append(line)
         newlines = copy.copy(lines)
-        self._content = newlines #cStringIO.StringIO(''.join(newlines))
+        self.content = newlines #cStringIO.StringIO(''.join(newlines))
         environ['wsgi.input'] = None
-
-    @property
-    def content(self):
-        """ Get the body of request as a stream """
-        return self._content
-
-    @property
-    def environ(self):
-        """ Access the environ  """
-        return self._environ
+        
+    def __setattr__(self, name, val):
+        if name in ('environ', 'content'):
+            raise Exception('Attribute %s is read only.' % name)
+        self.__dict__[name] = val
 
 class Response:
-
+    """ """
+    
     _STATUS_LIST = {
                         0: '',
                         200:'200 OK',
@@ -37,50 +33,26 @@ class Response:
 
     def __init__(self, start_response, status=0, header=None, body=None):
         
-        self._start_response = start_response
-        
-        self.status = status
-        
+        self._start_response = start_response        
+        self.status = status        
         if (header==None):
-            self._header = []
+            self.header = []
         else:
-            self._header = [].extend(header)
-
+            self.header = [].extend(header)
         if (body==None):
-            self._body = []
+            self.body = []
         else:
-            self._body = [].extend(body)
-    
-    @property
-    def statusList(self):
-        """ List of HTTP satus keys """
-        return Response._STATUS_LIST.keys()
+            self.body = [].extend(body)
 
-    @property
-    def statusString(self):
-        return Response._STATUS_LIST[self.status]
-
-    @property
-    def status(self):
-        """ HTTP status integer defined by Response.statusList """
-        return self._status
-
-    @status.setter
-    def status(self, value):
-        if (not value in Response._STATUS_LIST):
+    def __setattr__(self, name, val):
+        if (name in ('status')) and (not value in Response._STATUS_LIST):
             raise Exception('HTTP status %s is not defined' % value)
-        self._status = value
+        self.__dict__[name] = val
 
-    @property
-    def header(self):
-        return self._header
-
-    @property
-    def body(self):
-        return self._body
-
-    @property
-    def contentLength(self):
+    def get_status_string(self):
+        return Response._STATUS_LIST[self.status]
+    
+    def get_content_length(self):
         l = 0
         for i in self._body:
             l += len(i)
@@ -89,20 +61,19 @@ class Response:
     def return_response(self):
         if (self.status == 0):
             raise Exception('HTTP status is unset')
-
-        self._start_response(self.statusString, self.header)
+        self._start_response(self.get_status_string(), self.header)
         return self.body
 
 class Context:
+    """ """
 
     def __init__(self, request, response):
-        self._request = request
-        self._response = response
-
-    @property
-    def request(self):
-        return self._request
-
-    @property
-    def response(self):
-        return self._response
+        # TODO: fix following
+        self.__dict__['request'] = request
+        self.__dict__['response'] = response
+    
+    def __setattr__(self,name, val):
+        if name in ('request', 'response'):
+            raise Exception('Attribute %s is read only.' % name)
+        self.__dict__[name] = val    
+            
